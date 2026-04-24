6ef56e9b4c0e22077b8b956ef5f0ff385ba30aab
Gustavo Rezende <gustavo.rezende@elogroup.com.br>
2026-04-23 22:12:45 -0300

    banco de dados added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from abc import ABC , abstractmethod
+from pathlib import Path
 
 class Emprestimo(ABC):
     def __init__(self):
@@ -9,18 +10,74 @@ class Emprestimo(ABC):
 
 class EmprestimoLivro(Emprestimo):
     def __init__(self, nome_usuario, titulo):
-        super().__init__()
-        self.nome_usuario = nome_usuario
+        super().__init__(nome_usuario)
         self.titulo = titulo
 
+    def to_dict(self):
+        return {
+           "usuario": self.nome_usuario,
+           "tipo": self.titulo
+        }
+    
     def registrar(self):
         pass
 
 class EmprestimoRevista(EmprestimoLivro):
-    def __init__(self, edicao):
-        super().__init__()
+    def __init__(self, nome_usuario, edicao):
+        super().__init__(nome_usuario)
         self.edicao = edicao
+    
+    def to_dict(self):
+        return {
+           "usuario": self.nome_usuario,
+           "tipo": self.titulo
+        }
+        
 
     def registrar(self):
         pass
 
+class BancoDeDados:
+    def __init__(self):
+        self._emprestimos = []
+
+    def adicionar(self, emprestimo: Emprestimo):
+        self._emprestimos.append(emprestimo)
+
+    def salvar(self, nome_arquivo):
+        caminho = Path(nome_arquivo)
+        conteudo = self.extrair_dados()
+        caminho.write_text(conteudo, encoding="utf-8")
+        print(f"Arquivo salvo em {caminho.resolve()}")
+
+    def extrair_dados(self) -> str:
+        if not self._emprestimos:
+            return "Banco Vazio"
+        
+        linhas =[]
+
+        for emprestimo in self._emprestimos:
+            dados = emprestimo.to_dict()
+            linha = " | ".join(f"{k}:{v}" for k, v in dados.items())
+            linhas.append(linha)
+
+            return "\n".join(linhas)
+        
+
+def main():
+    emprestimo1 = EmprestimoLivro("Gustavo", "O Samurai")
+    emprestimo2 = EmprestimoLivro("Rafel", "O Labubu") 
+    emprestimo3 = EmprestimoLivro("Gustavo", "O Samurai")
+    emprestimo4 = EmprestimoRevista("Outro Gustavo", "O clone")
+    emprestimo5 = EmprestimoRevista("O mesmo Rafael", "Rei do BrainRot")
+    emprestimo6 = EmprestimoRevista("Outro Gustavo", "O clone")
+
+    banco = BancoDeDados()
+
+    emprestimos = [emprestimo1,emprestimo2,emprestimo3,emprestimo4,emprestimo5,emprestimo6]
+
+    for emprestimo in emprestimos:
+        banco.adicionar(emprestimo) 
+        banco.salvar("emprestimos.txt")
+
+main()
